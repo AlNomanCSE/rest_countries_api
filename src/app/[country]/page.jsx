4e15758cf968fde18d649b1e305ac2a69d6f6e21
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-03-16 12:11:40 +0600

    ---

--- src/app/[country]/page.jsx
+++ src/app/[country]/page.jsx
@@ -16,9 +16,7 @@ const jsonData = fs.readFileSync(filePath, "utf8");
 // Parse the JSON data into a JavaScript object
 const datas = JSON.parse(jsonData);
 const page = ({ params }) => {
-  const [Country] = datas.filter((data) =>
-    data.name.toLowerCase().includes(params.country.toLowerCase())
-  );
+  const [Country] = datas.filter((data) => data.name === params.country);
 
   return (
     <>
@@ -35,7 +33,7 @@ const page = ({ params }) => {
         style={{ maxWidth: "500px", margin: "0 auto" }}
       >
         <Image
-          src={Country.flags.svg || Country.flags.png}
+          src={Country.flags.svg}
           alt="country image"
           width={200}
           height={200}
